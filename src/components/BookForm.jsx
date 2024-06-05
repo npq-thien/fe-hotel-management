db94ccd98b8d286c452b33aad4a2cdb926f3fc07
npq-thien <thiennguyen3132002@gmail.com>
2024-06-05 17:20:21 +0700

    done logic for booking form

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,46 +1,72 @@
-import React from 'react';
-import Dropdown from './Dropdown'; // Ensure you have imported the Dropdown component correctly
+import React, { useEffect, useState } from "react";
+import Dropdown from "./Dropdown"; // Ensure you have imported the Dropdown component correctly
+
+const dropdownRooms = [
+  { label: "Single bedroom", href: "#" },
+  { label: "Double bedroom", href: "#" },
+  { label: "Deluxe Room", href: "#" },
+  { label: "Family Room", href: "#" },
+  { label: "Suite", href: "#" },
+];
+
+const dropdownGuests = [
+  { label: "1", href: "#" },
+  { label: "2", href: "#" },
+  { label: "3", href: "#" },
+  { label: "4", href: "#" },
+  { label: "5", href: "#" },
+];
 
 const App = () => {
-  const dropdownRooms = [
-    { label: 'Single bedroom', href: '#' },
-    { label: 'Double bedroom', href: '#' },
-    { label: 'Deluxe Room', href: '#' },
-    { label: 'Family Room', href: '#' },
-    { label: 'Suite', href: '#' },
-  ];
+  const [checkInDate, setCheckInDate] = useState("");
+  const [checkOutDate, setCheckOutDate] = useState("");
+  const [roomType, setRoomType] = useState("Single bedroom");
+  const [selectedGuest, setSelectedGuest] = useState(1);
 
-  const dropdownGuests = [
-    { label: '1', href: '#' },
-    { label: '2', href: '#' },
-    { label: '3', href: '#' },
-    { label: '4', href: '#' },
-    { label: '5', href: '#' },
-  ];
+  // useEffect(() => {
+  //   console.log("log here ", selectedGuest, roomType);
+  // }, [selectedGuest, roomType]);
 
   return (
     <div className="flex justify-center">
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 max-w-6xl gap-2 bg-gray-800 text-white p-4">
         <div className="flex flex-col rounded-xl items-center justify-center border border-stone-700 gap-2 p-5">
           CHECK-IN
-          <input type="date" className="text-black p-1 rounded-md " />
+          <input
+            value={checkInDate}
+            onChange={(event) => setCheckInDate(event.target.value)}
+            type="date"
+            className="text-black p-1 rounded-md "
+          />
         </div>
         <div className="flex flex-col rounded-md items-center justify-center border border-stone-700 gap-2 p-5">
           CHECK-OUT
-          <input type="date" className="text-black p-1 rounded-md" />
+          <input
+            value={checkOutDate}
+            onChange={(event) => setCheckOutDate(event.target.value)}
+            type="date"
+            className="text-black p-1 rounded-md"
+          />
         </div>
         <div className="z-20 flex flex-col rounded-md items-center justify-center border border-stone-700 gap-2 p-5">
           ROOM TYPE
-          <Dropdown title="Single bedroom" items={dropdownRooms} />
+          <Dropdown
+            title={roomType}
+            items={dropdownRooms}
+            onChange={(event) => setRoomType(event.target.value)}
+            width={44}
+          />
         </div>
         <div className="z-10 flex flex-col rounded-md items-center justify-center border border-stone-700 gap-2 p-5">
           GUESTS
-          <Dropdown title="1" items={dropdownGuests} />
+          <Dropdown
+            title={selectedGuest}
+            items={dropdownGuests}
+            onChange={(event) => setSelectedGuest(event.target.value)}
+          />
         </div>
         <div className="flex flex-col rounded-md items-center justify-center bg-secondary gap-2 p-4 md:col-span-2 lg:p-5 lg:col-span-1 hover:cursor-pointer">
-          <button className="text-2xl font-semibold p-2 rounded">
-            CHECK AVAILABILITY
-          </button>
+          <button className="text-2xl font-semibold p-2 rounded">CHECK AVAILABILITY</button>
         </div>
       </div>
     </div>
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 
-const Dropdown = ({ title, items }) => {
+const Dropdown = ({ title, items, width, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(title);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -13,14 +14,14 @@ const Dropdown = ({ title, items }) => {
       <div>
         <button
           type="button"
-          className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-3 py-2 bg-white text-gray-700 focus:outline-none"
+          className={"inline-flex text-center justify-center w-" + width + " rounded-md border border-gray-300 shadow-sm px-3 py-2 bg-white text-gray-700 focus:outline-none"}
           id="options-menu"
           aria-expanded="true"
           aria-haspopup="true"
           onClick={toggleDropdown}
         >
           <div class="flex items-center gap-2">
-            {title}
+            {selectedValue ? selectedValue : title}
             <IoMdArrowDropdown />
           </div>
         </button>
@@ -40,6 +41,13 @@ const Dropdown = ({ title, items }) => {
                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 role="menuitem"
                 key={index}
+                onClick={() => {
+                  setSelectedValue(item.label);
+                  setIsOpen(false);
+                  // we mimics the target.value.item for the other component
+                  // can get this value when it change
+                  onChange({ target: { value: item.label } });
+                }}
               >
                 {item.label}
               </a>
